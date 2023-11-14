778c13ac11794672eb814f1ee143c68439dd512c
MarkitoRosas <marcoantonio.rosasgonzalez@utxicotepec.edu.mx>
2023-11-14 16:48:19 -0600

    bueno

--- Desarrollo_Frontend/frontend/src/app/Components/navbar/navbar.component.ts
+++ Desarrollo_Frontend/frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  // Agrega una propiedad para controlar el estado del menú
+  isMenuOpen = false;
 
+  // Define la función para alternar el estado del menú
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
