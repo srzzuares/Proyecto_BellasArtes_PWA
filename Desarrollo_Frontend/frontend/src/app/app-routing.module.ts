696945b570bb9442fbd6dbb8200ffab5cb4b5f0a
MarkitoRosas <marcoantonio.rosasgonzalez@utxicotepec.edu.mx>
2023-11-14 17:18:29 -0600

    bueno

--- Desarrollo_Frontend/frontend/src/app/app-routing.module.ts
+++ Desarrollo_Frontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from "./Components/about/about.component";
+import { CarruselComponent } from "./Components/carrusel/carrusel.component";
+// import { FooterComponent } from "./Components/footer/footer.component";
+import { HomeComponent } from "./Components/home/home.component";
+import { LoginComponent } from './Components/login/login.component';
+// import { NavbarComponent } from "./Components/navbar/navbar.component";
+import {  SignupComponent} from "./Components/signup/signup.component";
+import { CatalogoComponent } from "./Components/catalogo/catalogo.component";
+import { ArtistaComponent } from "./Components/artista/artista.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'about', component: AboutComponent},
+{path: 'carrusel', component: CarruselComponent},
+{path: 'home', component:HomeComponent},
+{path: 'login', component:LoginComponent},
+{path: 'signup', component:SignupComponent },
+{path: 'artista', component:ArtistaComponent},
+{path: 'catalogo', component:CatalogoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Desarrollo_Frontend/frontend/src/app/app.module.ts
+++ Desarrollo_Frontend/frontend/src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { CarruselComponent } from './Components/carrusel/carrusel.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AboutComponent } from './Components/about/about.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { CatalogoComponent } from './Components/catalogo/catalogo.component';
+import { ArtistaComponent } from './Components/artista/artista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CarruselComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    SignupComponent,
+    CatalogoComponent,
+    ArtistaComponent
   ],
   imports: [
     BrowserModule,
