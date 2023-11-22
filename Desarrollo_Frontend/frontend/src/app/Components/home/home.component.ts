47b59ecf9ba2b2e8cd05310ca62cbf497aaaea14
sr <srzzuares@gmail.com>
2023-11-21 18:36:37 -0600

    Rehacer el home

--- Desarrollo_Frontend/frontend/src/app/Components/home/home.component.ts
+++ Desarrollo_Frontend/frontend/src/app/Components/home/home.component.ts
@@ -1,45 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { artistaService } from 'src/app/services/artista-serv.service';
-import { CatalogoService } from 'src/app/services/catalogo.service';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  artistas: any[] = []; // Array para almacenar los datos de las artesanías
-  catalogo: any[] = []; // Array para almacenar las joyerías
+export class HomeComponent {
 
-  constructor(
-    private artista: artistaService,
-    private pintura: CatalogoService,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    //this.loadpintura();
-    this.loadArtista();
-  }
-
-  loadArtista() {
-    this.artista.GetArtista().subscribe((data) => {
-      this.artistas = data; 
-    });}
-    
-  // loadpintura() {
-  //   this.pintura.getRopaArtesanales().subscribe((data) => {
-  //     this.catalogo = data.slice(0, 3); 
-  //   });
-  // }
-  
-  //}
-
-  navigateToartistas(id: number) {
-    this.router.navigate(['/artista/', id]); // Redirige al usuario al componente "ropa-pieza" con el ID
-  }
-  // navigateToJpyeriaPieza(id: number) {
-  //   this.router.navigate(['/joyeria-pieza', id]); // Redirige al usuario al componente "ropa-pieza" con el ID
-  // }
 }
